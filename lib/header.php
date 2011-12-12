7dcdbe707c59017f0ebeff783c0bf878ead89178
Dmitry Granovsky <dima.granovsky@gmail.com>
2011-12-12 19:13:32 +0000

    db connect added

--- lib/header.php
+++ lib/header.php
@@ -14,4 +14,10 @@ $smarty->template_dir = $config['smarty']['template_dir'];
 $smarty->compile_dir  = $config['smarty']['compile_dir'];
 $smarty->config_dir   = $config['smarty']['config_dir'];
 $smarty->cache_dir    = $config['smarty']['cache_dir'];
+
+$db = mysql_connect($config['mysql']['host'], $config['mysql']['user'], $config['mysql']['passwd']) or die ("Unable to connect to mysql server");
+if (!sql_query("USE ".$config['mysql']['dbname'], 0, 1)) {
+    die ("Unable to open mysql database");
+}
+mysql_query("SET names utf8");
 ?>
