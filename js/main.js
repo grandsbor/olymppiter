0dd453b53f78d5dd68b3708e0ed263099a42d178
Maria Nikolaeva <mary.nikolaeva@gmail.com>
2011-12-14 15:37:44 +0000

    some css

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function Field() {
 }
 
 Field.prototype.createHtml = function(data) {
-   var html = '<td class="marktable-td-mark"><input name="mark[' + data.subtask + ']" value="' + (data.value ? data.value : '') + '" data-subtask="' + data.subtask + '" data-type="mark"></td>';
+   var html = '<td class="marktable-td-mark"><input name="mark[' + data.subtask + ']" value="' + (data.value ? data.value : '') + '" data-subtask="' + data.subtask + '" data-type="mark" class="marktable-mark"></td>';
    return html;
 }
 
@@ -17,20 +17,20 @@ Field.prototype.check = function() {
       
       var $col = $("#col-" + this.fieldNode.data('subtask'));
       if(this.fieldNode.val() > $col.data('max')) {
-         this.fieldNode.addClass('marktable-td-error').attr('title','Значение выше максимального');
+         this.fieldNode.addClass('error').attr('title','Значение выше максимального');
          return false;
       }
-      else if(this.fieldNode.hasClass('marktable-td-error')) {
-         this.fieldNode.removeClass('marktable-td-error').attr('title','');
+      else if(this.fieldNode.hasClass('error')) {
+         this.fieldNode.removeClass('error').attr('title','');
       }
    }
    else {
       if(/[0-9]{1}-[0-9]{2}[a-zA-Z]{3}-[0-9]{1}/.test(this.fieldNode.val()) == false) {
-         this.fieldNode.addClass('marktable-td-error').attr('title','Некорректный код');
+         this.fieldNode.addClass('error').attr('title','Некорректный код');
          return false;
       }
-      else if(this.fieldNode.hasClass('marktable-td-error')) {
-         this.fieldNode.removeClass('marktable-td-error').attr('title','');
+      else if(this.fieldNode.hasClass('error')) {
+         this.fieldNode.removeClass('error').attr('title','');
       }
    }
    return true;
@@ -140,6 +140,7 @@ function Table(data) {
    }
    else {
       this.tableNode = $(this.data.node);
+      this.tableNode.addClass('marktable');
       if(!data.cols) {
          alert('Нет колонок');
       }
@@ -151,12 +152,21 @@ function Table(data) {
          this.createRows();
       }
       
-      this.tableNode.delegate('input','blur',function(event){
+      this.tableNode.delegate('input','focus',function(event){
         
-         var field = new Field($(event.target));
+         $(event.target).addClass('active');
+      });
+      
+      this.tableNode.delegate('input','blur',function(event){
+	      
+	 var $input =  $(event.target);
+	 
+	 $input.removeClass('active');
+        
+         var field = new Field($input);
          field.check();
          
-         var row = new Row($(event.target).closest('tr'));
+         var row = new Row($input.closest('tr'));
          row.recount();
       });
       
@@ -182,10 +192,11 @@ function Table(data) {
 Table.prototype.createCols = function() {
    this.tableNode.append('<thead><tr id="table-header"></tr></thead>');
    var $header = $('#table-header');
-   $("<th>Код</th>").appendTo($header);
+   $("<th class='marktable-code-col'>Код</th>").appendTo($header);
    for(i=0;i<this.data.cols.length;++i) {
       $("<th id='col-" + this.data.cols[i].id + "' class='marktable-mark-col' data-id='" + this.data.cols[i].id +"' data-max='" + this.data.cols[i].max +"'>"+ this.data.cols[i].name +" (max " + this.data.cols[i].max + ")</th>").appendTo($header);
    }
+   $("<th class='marktable-sum-col'>Итого</th>").appendTo($header);
 }
 Table.prototype.createRows = function() {
    var i = 0;
