326398fa6d5ac3c0d49aa6cd23d704fda7ee7b8b
Dmitry Granovsky <dima.granovsky@gmail.com>
2018-02-11 22:09:39 +0000

    fix at least some mysql problems

--- lib/lib_main.php
+++ lib/lib_main.php
@@ -3,16 +3,19 @@
 function strip_zero($n) {
     return $n == (int)$n ? (int)$n : $n;
 }
-function get_task_list() {
+function fetch($query) {
     global $pdo_db;
+    return $pdo_db->query($query)->fetchAll();
+}
+function get_task_list() {
     $out = array();
-    $res = $pdo_db->query("SELECT contest_id, contest_name FROM contests WHERE parent_id = 0 ORDER BY contest_id");
+    $res = fetch("SELECT contest_id, contest_name FROM contests WHERE parent_id = 0 ORDER BY contest_id");
     //collect tasks
     foreach ($res as $r) {
         $contest = array('id' => $r['contest_id'], 'name' => $r['contest_name']);
-        $res1 = $pdo_db->query("SELECT task_id, task_name, status FROM tasks WHERE contest_id = ".$r['contest_id']." ORDER BY task_id");
+        $res1 = fetch("SELECT task_id, task_name, status FROM tasks WHERE contest_id = ".$r['contest_id']." ORDER BY task_id");
         //tasks may be attached here
-        if ($res1->rowCount() > 0) {
+        if (count($res1) > 0) {
             foreach ($res1 as $r1) {
                 $contest['tasks'][] = array(
                     'id' => $r1['task_id'],
@@ -25,10 +28,10 @@ function get_task_list() {
             continue;
         }
         //or they may be attached lower
-        $res1 = $pdo_db->query("SELECT contest_id, contest_name FROM contests WHERE parent_id = ".$r['contest_id']." ORDER BY contest_id");
+        $res1 = fetch("SELECT contest_id, contest_name FROM contests WHERE parent_id = ".$r['contest_id']." ORDER BY contest_id");
         foreach ($res1 as $r1) {
             $round = array('id' => $r1['contest_id'], 'name' => $r1['contest_name']);
-            $res2 = $pdo_db->query("SELECT task_id, task_name FROM tasks WHERE contest_id = ".$r1['contest_id']." ORDER BY task_id");
+            $res2 = fetch("SELECT task_id, task_name FROM tasks WHERE contest_id = ".$r1['contest_id']." ORDER BY task_id");
             foreach ($res2 as $r2) {
                 $round['tasks'][] = array(
                     'id' => $r2['task_id'],
@@ -44,9 +47,8 @@ function get_task_list() {
     return $out;
 }
 function get_judges_for_task($task_id) {
-    global $pdo_db;
     //returns array of assoc arrays type (id, name, email)
-    $res = $pdo_db->query("SELECT * FROM judges WHERE judge_id IN (SELECT judge_id FROM judges_by_tasks WHERE task_id = $task_id)");
+    $res = fetch("SELECT * FROM judges WHERE judge_id IN (SELECT judge_id FROM judges_by_tasks WHERE task_id = $task_id)");
     $out = array();
     foreach ($res as $r) {
         $out[] = array('id' => $r['judge_id'], 'name' => $r['judge_name'], 'email' => $r['judge_email']);
@@ -55,17 +57,17 @@ function get_judges_for_task($task_id) {
 }
 function get_contest_by_task($task_id, $highest = false) {
     global $pdo_db;
-    $res = $pdo_db->query("SELECT contest_id FROM tasks WHERE task_id = $task_id LIMIT 1");
-    if (!$res->rowCount()) return false;
+    $res = fetch("SELECT contest_id FROM tasks WHERE task_id = $task_id LIMIT 1");
+    if (!count($res)) return false;
     
-    $r = $res->fetchAll()[0];
+    $r = $res[0];
     if (!$highest)
         return $r['contest_id'];
 
     $pid = $r['contest_id'];
     while ($pid > 0) {
-        $tr = $pdo_db->query("SELECT parent_id FROM contests WHERE contest_id = $pid LIMIT 1");
-        $r = $tr->fetchAll()[0];
+        $tr = fetch("SELECT parent_id FROM contests WHERE contest_id = $pid LIMIT 1");
+        $r = $tr[0];
         if ($r['contest_id'] == 0) return $pid;
         $pid = $r['contest_id'];
     }
@@ -124,9 +126,9 @@ function get_subtasks($task_id) {
 function get_temporary_marks($task_id, $judge_id) {
     global $pdo_db;
     $marks = array();
-    $res = $pdo_db->query("SELECT solution_id, code FROM solutions_tmp WHERE task_id=$task_id AND judge_id=$judge_id");
+    $res = fetch("SELECT solution_id, code FROM solutions_tmp WHERE task_id=$task_id AND judge_id=$judge_id");
     foreach ($res as $r) {
-        $res1 = $pdo_db->query("SELECT mark_id, subtask_id, mark_value FROM marks_tmp WHERE solution_id=".$r['solution_id']." ORDER BY subtask_id");
+        $res1 = fetch("SELECT mark_id, subtask_id, mark_value FROM marks_tmp WHERE solution_id=".$r['solution_id']." ORDER BY subtask_id");
         $t = array();
         foreach ($res1 as $r1) {
             $t[$r1['subtask_id']] = array('id' => $r1['mark_id'], 'value' => strip_zero($r1['mark_value']), 'subtask' => $r1['subtask_id']);
@@ -144,13 +146,12 @@ function save_temporary_marks($task_id, $judge_id, $marks) {
     // $marks is an array type (code, array of arrays type (subtask_id => value))
     if (!$task_id || !$judge_id) return "Bad task id or judge id";
     //TODO: check if this judge can judge this task
-    $pdo_db->query("START TRANSACTION");
+    $pdo_db->beginTransaction();
     $ret_codes = array();
-    foreach($marks as $solution) {
+    foreach ($marks as $solution) {
         $code = strtoupper(trim($solution['code']));
         //check the code for validity
         if (!preg_match('/^[0-9]\-[0-9]{2}[A-Z]{3}\-[0-9]$/', $code)) return "Bad code format";
-        $code = $pdo_db->quote($code);
         if ($solution['id']) {
             $solution['id'] = (int)$solution['id'];
             //the code may have changed
@@ -166,25 +167,25 @@ function save_temporary_marks($task_id, $judge_id, $marks) {
         }
         $ret_codes[] = $solution['id'];
 
-        foreach($solution['marks'] as $subtask_id => $value) {
+        foreach ($solution['marks'] as $subtask_id => $value) {
             $subtask_id = (int)$subtask_id;
             $value = (float)$value;
-            $res = $pdo_db->query("SELECT mark_id FROM marks_tmp WHERE solution_id=".$solution['id']." AND subtask_id=$subtask_id LIMIT 1");
-            if ($res->rowCount() > 0) {
-                $r = $res->fetchAll()[0];
+            $res = fetch("SELECT mark_id FROM marks_tmp WHERE solution_id=".$solution['id']." AND subtask_id=$subtask_id LIMIT 1");
+            if (count($res) > 0) {
+                $r = $res[0];
                 $pdo_db->query("UPDATE marks_tmp SET mark_value=$value WHERE mark_id=".$r['mark_id']." LIMIT 1");
             } else {
                 $pdo_db->query("INSERT INTO marks_tmp VALUES (NULL, $subtask_id, ".$solution['id'].", $value)");
             }
         }
     }
-    $pdo_db->query("COMMIT");
+    $pdo_db->commit();
     return $ret_codes;
 }
 function delete_temporary_solution($solution_id) {
     global $pdo_db;
     if (!$solution_id) return false;
-    if (!$pdo_db->query("DELETE FROM solutions_tmp WHERE solution_id = $solution_id LIMIT 1")) return false;
+    $pdo_db->query("DELETE FROM solutions_tmp WHERE solution_id = $solution_id LIMIT 1");
     return true;
 }
 function get_aggregate_marks($task_id) {
@@ -193,7 +194,7 @@ function get_aggregate_marks($task_id) {
     $out_aggr = array();
     $aggr_codes = array();
     //already aggregated
-    $res = $pdo_db->query("SELECT solution_id, s.contestant_id, code FROM solutions s LEFT JOIN contestants USING(contestant_id) WHERE task_id = $task_id ORDER BY code");
+    $res = fetch("SELECT solution_id, s.contestant_id, code FROM solutions s LEFT JOIN contestants USING(contestant_id) WHERE task_id = $task_id ORDER BY code");
     foreach ($res as $r) {
         $out_aggr[$r['code']]['contestant_id'] = (int)$r['contestant_id'];
         $res1 = $pdo_db->query("SELECT subtask_id, mark_value FROM final_marks WHERE solution_id = ".$r['solution_id']." ORDER BY subtask_id");
@@ -203,7 +204,7 @@ function get_aggregate_marks($task_id) {
         $aggr_codes[] = $r['code'];
     }
     //not yet aggregated
-    $res = $pdo_db->query("
+    $res = fetch("
         SELECT solution_id, judge_id, t.code, contestant_id
         FROM solutions_tmp t
         LEFT JOIN contestants USING (code)
@@ -236,7 +237,7 @@ function save_aggregate_string($task_id, $contestant_id, $code, $marks) {
     if (!$task_id || !is_array($marks) || !$marks) return "No task id given or empty marks";
     
     //it may be an unknown contestant, let's create him then
-    $pdo_db->query("START TRANSACTION");
+    $pdo_db->beginTransaction();
     if ($contestant_id == -1) {
         //but first we have to know contest id
         $contest_id = get_contest_by_task($task_id, true);
@@ -247,18 +248,18 @@ function save_aggregate_string($task_id, $contestant_id, $code, $marks) {
     }
 
     //perhaps we need to create a new solution (if it is the first saving)
-    $res = $pdo_db->query("SELECT solution_id FROM solutions WHERE task_id = $task_id AND contestant_id = $contestant_id LIMIT 1");
-    if ($res->rowCount() == 0) {
+    $res = fetch("SELECT solution_id FROM solutions WHERE task_id = $task_id AND contestant_id = $contestant_id LIMIT 1");
+    if (count($res) == 0) {
         $pdo_db->query("INSERT INTO solutions VALUES (NULL, $task_id, $contestant_id, '')");
         $solution_id = $pdo_db->lastInsertId();
     } else {
-        $r = $res->fetchAll()[0];
+        $r = $res[0];
         $solution_id = $r['solution_id'];
     }
 
     //save marks (checking whether they exist)
     $old_marks = array();
-    $res = $pdo_db->query("SELECT mark_id, subtask_id FROM final_marks WHERE solution_id = $solution_id");
+    $res = fetch("SELECT mark_id, subtask_id FROM final_marks WHERE solution_id = $solution_id");
     foreach ($res as $r) {
         $old_marks[$r['subtask_id']] = $r['mark_id'];
     }
@@ -271,14 +272,14 @@ function save_aggregate_string($task_id, $contestant_id, $code, $marks) {
             $pdo_db->query("INSERT INTO final_marks VALUES (NULL, $sid, $solution_id, ".(float)$val.")");
         }
     }
-    $pdo_db->query("COMMIT");
+    $pdo_db->commit();
     return $contestant_id;
 }
 function get_task_result($task_id) {
     global $pdo_db;
     $contest_id = get_contest_by_task($task_id);
     
-    $res = $pdo_db->query("select contestant_id, ceil(sum(mark_value)) as mark from solutions left join final_marks using(solution_id) where task_id = $task_id group by solution_id");
+    $res = fetch("select contestant_id, ceil(sum(mark_value)) as mark from solutions left join final_marks using(solution_id) where task_id = $task_id group by solution_id");
     $solutions = array();
     foreach ($res as $r) {
         $solutions[$r['contestant_id']] = $r['mark'];
